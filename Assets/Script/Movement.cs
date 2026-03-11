77d0746661da86b2738dd3aa3a2de37de1690937
rifkiagungx13-alt <rifkiagungx13@gmail.com>
2026-03-11 20:33:31 +0700

    Tahap 1 (Speed and jump)

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,28 +1,41 @@
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.InputSystem;
 
-public enum Speeds {Slow= 0, Normal= 1, Fast= 2, Faster= 3, Fastest= 4};
+public enum Speeds {Slow=0, Normal=1, Fast=2, Faster=3, Fastest=4}
+
 public class Movement : MonoBehaviour
 {
     public Speeds CurrentSpeed;
-    //                      0       1      2      3      4
-    float[] SpeedValues = {8.6f, 10.4f, 12.96f, 15.6f, 19.27f};
+
+    float[] SpeedValues = {8.6f,10.4f,12.96f,15.6f,19.27f};
+
     public Transform GroundCheckTransform;
     public float GroundCheckRadius;
     public LayerMask GroundMask;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         transform.position += Vector3.right * SpeedValues[(int)CurrentSpeed] * Time.deltaTime;
 
-        if(Input.GetMouseButton(0))
+        if(Mouse.current.leftButton.wasPressedThisFrame)
         {
-            //Jump
+            if(OnGround())
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.AddForce(Vector2.up * 26.6f, ForceMode2D.Impulse);
+            }
         }
     }
 
     bool OnGround()
     {
-        return Physics2D.OverlapCircle();
+        return Physics2D.OverlapCircle(GroundCheckTransform.position, GroundCheckRadius, GroundMask);
     }
 }
