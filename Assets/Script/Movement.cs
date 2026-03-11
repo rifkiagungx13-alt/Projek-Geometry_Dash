ea43ce43269a631b791c98560b5d4a25252fd51b
rifkiagungx13-alt <rifkiagungx13@gmail.com>
2026-03-11 19:47:02 +0700

    getignore

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public enum Speeds {Slow= 0, Normal= 1, Fast= 2, Faster= 3, Fastest= 4};
+public class Movement : MonoBehaviour
+{
+    public Speeds CurrentSpeed;
+    //                      0       1      2      3      4
+    float[] SpeedValues = {8.6f, 10.4f, 12.96f, 15.6f, 19.27f};
+    public Transform GroundCheckTransform;
+    public float GroundCheckRadius;
+    public LayerMask GroundMask;
+
+    void Update()
+    {
+        transform.position += Vector3.right * SpeedValues[(int)CurrentSpeed] * Time.deltaTime;
+
+        if(Input.GetMouseButton(0))
+        {
+            //Jump
+        }
+    }
+
+    bool OnGround()
+    {
+        return Physics2D.OverlapCircle();
+    }
+}
